814ae5a7dc388bc2113084082bf6b299532f9da1
sakeev92 <vs.sakeev@mail.ru>
2023-04-10 17:39:34 +0300

    HW

---
+++ src/main/java/com/StreamAPIandOptional/StreamApiAndOptionalApplication.java
@@ -0,0 +1,13 @@
+package com.StreamAPIandOptional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiAndOptionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamApiAndOptionalApplication.class, args);
+	}
+
+}
